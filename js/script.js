9a3affd7e22d31198951b1d6d1b7d7944f64b3db
phalcobtw <laxz_16@hotmail.com>
2023-11-27 20:41:15 -0700

    scripts para mostrar menu

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const navbar = document.querySelector(".navbar");
+const openMenuButton = document.querySelector(".menu-button");
+const closeMenuButton = document.querySelector(".menu-button-close");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(window.innerWidth);
+});
+
+window.addEventListener("resize", onresize);
+
+openMenuButton.addEventListener("click", () => {
+  navbar.classList.add("open");
+  navbar.style.display = "block";
+});
+
+closeMenuButton.addEventListener("click", () => {
+  navbar.classList.remove("open");
+  navbar.style.display = "none";
+});
+
+var onresize = function () {
+  let width = document.body.clientWidth;
+  if (width >= 600) {
+    navbar.style.display = "flex";
+  }
+};
