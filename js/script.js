cc58760b60302eba046af4734ffd592df1289464
phalcobtw <laxz_16@hotmail.com>
2023-11-29 17:30:52 -0700

    header fijo en scroll para mobile y desktop view

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector(".navbar");
 const openMenuButton = document.querySelector(".menu-button");
 const closeMenuButton = document.querySelector(".menu-button-close");
+const header = document.querySelector(".header");
+const mainContent = document.querySelector(".main-content");
+const body = document.querySelector("body");
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log(window.innerWidth);
@@ -26,3 +29,16 @@ var onresize = function () {
     navbar.style.display = "none";
   }
 };
+
+window.addEventListener("scroll", () => {
+  if (
+    mainContent.getBoundingClientRect().top < 0 /* &&
+    document.body.clientWidth < 600 */
+  ) {
+    header.classList.add("fijo");
+    body.classList.add("body-scroll");
+  } else {
+    header.classList.remove("fijo");
+    body.classList.remove("body-scroll");
+  }
+});
