1a7cf62fa68ddfc7df4c0dc7e1b7b9e6a6beb1f8
phalcobtw <laxz_16@hotmail.com>
2023-12-06 13:12:15 -0700

    bgmenu clickeable para sustituir closemenu button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const closeMenuButton = document.querySelector(".menu-button-close");
 const header = document.querySelector(".header");
 const mainContent = document.querySelector(".main-content");
 const body = document.querySelector("body");
+const bgmenu = document.querySelector(".backgroundmenu");
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log(window.innerWidth);
@@ -14,17 +15,29 @@ window.addEventListener("resize", onresize);
 openMenuButton.addEventListener("click", () => {
   navbar.classList.add("open");
   navbar.style.display = "block";
+  bgmenu.classList.add("bgopen");
+  bgmenu.style.display = "block";
+});
+
+bgmenu.addEventListener("click", () => {
+  navbar.classList.remove("open");
+  navbar.style.display = "none";
+  bgmenu.classList.remove("bgopen");
+  bgmenu.style.display = "none";
 });
 
 closeMenuButton.addEventListener("click", () => {
   navbar.classList.remove("open");
   navbar.style.display = "none";
+  bgmenu.classList.remove("bgopen");
+  bgmenu.style.display = "none";
 });
 
 var onresize = function () {
   let width = document.body.clientWidth;
   if (width >= 600) {
     navbar.style.display = "flex";
+    /* bgmenu.style.display = "none"; */
   } else {
     navbar.style.display = "none";
   }
